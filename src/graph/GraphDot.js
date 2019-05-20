5374d7df2cb6d9ee24a76d4bbc0e73bc56cf537d
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-05-20 14:29:07 -0700

    add roses to dots component

--- src/graph/GraphDot.js
+++ src/graph/GraphDot.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import Moment from 'moment';
+import rose from './../../assets/rose.png';
 
 function GraphDot(props){
   const now = new Moment();
@@ -8,8 +9,8 @@ function GraphDot(props){
 
   const heightPercent = (Dimensions.get('window').height * .9) / 100;
   const widthPercent = Dimensions.get('window').width / 100;
-  const width = 30 + days;
-  const height = 30 + days;
+  const width = 50 + days;
+  const height = 50 + days;
   const top = (heightPercent * props.y) - height;
   const left = (widthPercent * props.x) - width;
   const opacity = (100 + (days * 3)) / 100;
@@ -22,15 +23,12 @@ function GraphDot(props){
       top: top,
       left: left,
       opacity: opacity,
-
-      backgroundColor: 'green',
-      borderRadius: 100,
     }
   })
 
   return(
     <View>
-      <View style={styles.dot}></View>
+      <Image source={rose} style={styles.dot}/>
     </View>
   );
 }
